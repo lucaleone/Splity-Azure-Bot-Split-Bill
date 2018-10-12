d3c07e124dd442865170fe894e1a1b7eaefa6de3
Luca Leone <LLeone@suitsupply.com>
2018-10-12 15:52:03 +0200

    Init commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=lucalone-splity-bot-Web-Deploy.pubxml -p:Password=vmxMrJdtwizdJJkanq0ENSwsFnXq69JEohA2JqcXNx32wdzb5GxZlXyDTiWq
+
